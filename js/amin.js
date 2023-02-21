bf91606459a6e5f84ef82ae3de123a33250c9730
orifyuldashev <orif4ikstream@gmail.com>
2023-02-21 14:35:18 +0500

    salom

---
+++ js/amin.js
@@ -0,0 +1,124 @@
+// Begin1
+
+// let a = 10;
+// let P = 4 * a; 
+
+// console.log(P);
+
+
+// Begin2
+
+// let a = 4;
+// let b = a ** 2;
+
+// console.log(b);
+
+// Begin3
+
+// let a = 5;
+// let b = 8;
+// let S = a * b;
+
+// let P = 2 * (a + b)
+
+// console.log("To'rtburchakning yuzasi", S);
+// console.log ("To'rtburchakning peremetri", P);
+
+// Begin4
+
+// let d = 8;
+// let p = 3.14;
+// let L = d * p;
+
+// console.log(L);
+
+// Begin5
+
+// let a = 9;
+// let V = a ** 3;
+
+// let c = a ** 2;
+// let S = 6 * c;
+
+// console.log("Idishning litri", V);
+// console.log("Idishning yuzasi", S);
+
+// Begin6
+
+// let a = 5;
+// let b = 6;
+// let c = 7;
+
+// let V = (a * b * c);
+// let S = 2 * (a * b + b * c + a * c);
+
+// console.log("Idishning litri", V);
+// console.log("Idishning yuzasi", S);
+
+// Begin7
+
+// let R = 27;
+// let p = 3.14;
+// let L = 2 * p * R;
+
+// let S = p * (R ** 2)
+
+// console.log(S);
+// console.log(L);
+
+// Begin8
+
+// let a = 7;
+// let b = 8;
+// let c = (a + b) / 2;
+
+// console.log(c);
+
+// Begin9
+
+let a = 5;
+let b = 8;
+let c = (a * b) 2**;
+
+console.log(c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Begin23
+
+// let a = 2;
+// let b = 8;
+// let c = 4;
+
+// let d = b;
+// let g = c;
+// let j = a;
+
+// console.log(d);
+// console.log(g);
+// console.log(j);
+
+
+
+
+
+
